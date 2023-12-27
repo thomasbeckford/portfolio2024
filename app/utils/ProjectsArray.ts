71067c9e0abf7116cb039a5d17099bc2df58671a
Thomas Beckford <tebeckford@gmail.com>
2023-12-27 19:43:11 -0300

    fix: types

--- app/utils/ProjectsArray.ts
+++ app/utils/ProjectsArray.ts
@@ -1,34 +1,35 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react'
 
-const parseProjects = (mdContent) => {
-  const projects = [];
-  const lines = mdContent.split("\n");
+const parseProjects = (mdContent: string) => {
+  const projects = []
+  const lines = mdContent.split('\n')
 
   for (let i = 0; i < lines.length; i++) {
-    const line = lines[i];
+    const line = lines[i]
 
-    if (line.startsWith("## ")) {
-      const name = line.substr(3).trim();
-      const description = lines[++i].trim();
-      const imageLine = lines[++i];
-      const image = imageLine.match(/!\[(.*)\]\((.*)\)/)[2];
-      const tags = lines[++i].split(":")[1].trim();
-      const badges = [];
-      const buttons = [];
+    if (line.startsWith('## ')) {
+      const name = line.substr(3).trim()
+      const description = lines[++i].trim()
+      const imageLine = lines[++i]
+      const imageMatch = imageLine.match(/!\[(.*)\]\((.*)\)/)
+      const image = imageMatch ? imageMatch[2] : ''
+      const tags = lines[++i].split(':')[1].trim()
+      const badges = []
+      const buttons = []
 
-      while (lines[++i] && !lines[i].startsWith("- Badges:")) {}
-      while (lines[++i] && lines[i].startsWith("  - ")) {
-        const badgeLine = lines[i].substr(4).split("[");
-        const badgeName = badgeLine[0].trim();
-        const badgeColor = badgeLine[1].split("]")[0].trim();
-        badges.push({ text: badgeName, colorScheme: badgeColor });
+      while (lines[++i] && !lines[i].startsWith('- Badges:')) {}
+      while (lines[++i] && lines[i].startsWith('  - ')) {
+        const badgeLine = lines[i].substr(4).split('[')
+        const badgeName = badgeLine[0].trim()
+        const badgeColor = badgeLine[1].split(']')[0].trim()
+        badges.push({ text: badgeName, colorScheme: badgeColor })
       }
 
-      while (lines[++i] && lines[i].startsWith("  - ")) {
-        const buttonLine = lines[i].substr(4).split("[");
-        const buttonText = buttonLine[0].trim();
-        const buttonHref = buttonLine[1].split("]")[0].trim();
-        buttons.push({ text: buttonText, href: buttonHref });
+      while (lines[++i] && lines[i].startsWith('  - ')) {
+        const buttonLine = lines[i].substr(4).split('[')
+        const buttonText = buttonLine[0].trim()
+        const buttonHref = buttonLine[1].split(']')[0].trim()
+        buttons.push({ text: buttonText, href: buttonHref })
       }
 
       projects.push({
@@ -38,33 +39,34 @@ const parseProjects = (mdContent) => {
         tags: [tags],
         badges,
         buttons,
-      });
+      })
     }
   }
 
-  return projects;
-};
+  return projects
+}
 
 const ProjectsArray = () => {
-  const [projects, setProjects] = useState([]);
+  const [projects, setProjects] = useState([])
 
   useEffect(() => {
-    fetch("/content/Projects.md")
+    fetch('/content/Projects.md')
       .then((response) => {
         if (!response.ok) {
-          throw new Error("Failed to fetch markdown content");
+          throw new Error('Failed to fetch markdown content')
         }
-        return response.text();
+        return response.text()
       })
       .then((mdContent) => {
-        setProjects(parseProjects(mdContent));
+        const projects = parseProjects(mdContent) as any
+        setProjects(projects)
       })
       .catch((error) => {
-        console.error("Error fetching markdown content:", error);
-      });
-  }, []);
+        console.error('Error fetching markdown content:', error)
+      })
+  }, [])
 
-  return projects;
-};
+  return projects
+}
 
-export default ProjectsArray;
+export default ProjectsArray
